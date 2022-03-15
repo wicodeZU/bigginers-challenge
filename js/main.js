48af0b1785a175111778d1fb560e90ac570f7a2c
Carver <kerenketepela@gmail.com>
2022-03-15 15:56:34 +0300

    feat: navbar fn

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-document.querySelector("h1").textContent = "Kerenke"
+const navBar = () => {
+  return `<nav class="navbar">
+  <div class="left-nav">
+     <h1>TEP<em>ELA.</em></h1>
+  </div>
+  <div class="right-nav">
+      <ul class="nav-links">
+          <li>
+              <a href="/">About</a>
+          </li>
+          <li>
+              <a href="#">skills</a>
+          </li>
+          <li>
+              <a href="#">projects</a>
+          </li>
+          <li>
+              <a href="#">contact</a>
+          </li>
+      </ul>
+  </div>
+</nav>`
+}
+document.querySelector("nav").innerHTML = navBar()
